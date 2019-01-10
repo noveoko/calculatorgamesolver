49aff3a4a4da4c7182217f72209314fa48d44a27
Marcin Kraszewski <mrkraszewski@gmail.com>
2019-01-10 09:39:53 +0100

    built out the link collection function `fetch_all_links`. Need to make it less inefficient

--- wikibot/app.py
+++ wikibot/app.py
@@ -4,14 +4,19 @@ import re, json
 
 #return a single random wikipedia page
 def fetch_wiki_HTML(page_name=None):
+    html = ""
     assert (page_name), 'you must include a valid wikipedia page name'
     page_url = f'https://en.wikipedia.org/w/api.php?action=parse&page={page_name}&format=json'
     response = requests.get(page_url, timeout=10)
     if response.status_code == 200:
         data = response.json()
-        html = data['parse']['text']['*']
-        return html
-    
+        try:
+            html = data["parse"]["text"]["*"]
+            assert type(html) == str
+        except KeyError as ke:
+            html = str(data)
+    return html
+
 #return a single random wikipedia page title
 def fetch_random_pages(page_name=None):
     page_url = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=10&format=json'
@@ -31,36 +36,30 @@ def extract_links(page_title):
     soup = bs(raw_html, 'html.parser')
     paragraphs = [a for a in soup.findAll("p") if '<b>' in str(a)]
     for paragraph in paragraphs:
-        print(len(paragraph))
         just_text = re.sub(r"\(([\(].?)+\)", "", paragraph.text.lower())
-        print(just_text)
         links = paragraph.findAll("a")
         for link in links:
             try:
-                text = link.text        
+                text = link.text
                 url = link['href']
                 if '/wiki/' in url and ':' not in url and '#cite-note' not in url and text.lower() in just_text:
-                    all_links.append([url, text])
+                    short_url = url.replace('/wiki/',"")
+                    #all_links.append([url, text])
+                    all_links.append(short_url)
             except Exception as ee:
                 print(ee)
-    return all_links
+    return all_links[0]
 
-def collect_data(random_words=100, depth_per_search=20):
-    #{'Germany':['Central_Europe','Europe','Continent','Land#Land_mass',...]}
-    #Key is the randomly visited page, list contains all the terms in order they were collected
-    #stop collecting words when a loop is discovered
-    words_collected = {}
-    while len(words_collected.keys()) < random_words:
-        wiki_page_titles = fetch_random_pages()
-        for title in wiki_page_titles:
-            words_collected[title] = [] #create a new entry with empty list to hold results
-            new_wiki_link = extract_links(title)
-            if len(words_collected[title]) < depth_per_search: #check if words collected == depth_per_search
-                words_collected[title].append(new_wiki_link)
-            else:
-                pass
-    return words_collected
+def fetch_all_links(topic,depth=20):
+    collected = [topic]
+    while len(collected) < depth:
+        try:
+            target = collected[-1]
+            next_link = extract_links(target)
+            collected.append(next_link)
+            print(next_link)
+        except Exception as ee:
+            print(ee, "line 62")
 
 if __name__ == "__main__":
-    result = collect_data(5, 3)
-    print(result)
+    fetch_all_links('Germany',10)
--- wikibot/test_app.py
+++ wikibot/test_app.py
@@ -2,21 +2,18 @@ import app
 
 
 def test_fetch_wiki_HTML():
-    pass
+    data = app.fetch_wiki_HTML('Germany')
+    assert type(data) == str
 
 def test_fetch_random_pages():
-    pass
+    links = app.fetch_random_pages()
+    assert len(links) > 0
 
-def test_extract_first_link():
-    html = '''<p>The <b>baiji</b> (<a href="/wiki/Chinese_language" title="Chinese language">Chinese</a>: <span lang="zh"><a href="https://en.wiktionary.org/wiki/%E7%99%BD%E9%B1%80%E8%B1%9A" class="extiw" title="wiktionary:白鱀豚">白鱀豚</a></span>; <a href="/wiki/Pinyin" title="Pinyin">pinyin</a>: <i><span lang="zh-Latn-pinyin"><span class="unicode haudio"><span class="fn"><span style="white-space:nowrap;margin-right:.25em;"><a href="/wiki/File:Zh-bai2ji4tun2.ogg" title="About this sound"><img alt="About this sound" src="//upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Loudspeaker.svg/11px-Loudspeaker.svg.png" width="11" height="11" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Loudspeaker.svg/17px-Loudspeaker.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Loudspeaker.svg/22px-Loudspeaker.svg.png 2x" data-file-width="20" data-file-height="20"></a></span><a href="//upload.wikimedia.org/wikipedia/commons/7/7f/Zh-bai2ji4tun2.ogg" class="internal" title="Zh-bai2ji4tun2.ogg">báijìtún</a></span>&nbsp;<small class="metadata audiolinkinfo" style="cursor:help;">(<a href="/wiki/Wikipedia:Media_help" class="mw-redirect" title="Wikipedia:Media help"><span style="cursor:help;">help</span></a>·<a href="/wiki/File:Zh-bai2ji4tun2.ogg" title="File:Zh-bai2ji4tun2.ogg"><span style="cursor:help;">info</span></a>)</small></span></span></i>, <i>Lipotes vexillifer</i>, <i>Lipotes</i> meaning "left behind", <i>vexillifer</i> "flag bearer") is a functionally extinct species of <a href="/wiki/River_dolphin" title="River dolphin">freshwater dolphin</a> formerly found only in the <a href="/wiki/Yangtze_River" class="mw-redirect" title="Yangtze River">Yangtze River</a> in <a href="/wiki/China" title="China">China</a>. Nicknamed "Goddess of the Yangtze" (<a href="/wiki/Simplified_Chinese_characters" title="Simplified Chinese characters">simplified Chinese</a>: <span lang="zh-Hans">长江女神</span>; <a href="/wiki/Traditional_Chinese_characters" title="Traditional Chinese characters">traditional Chinese</a>: <span lang="zh-Hant">長江女神</span>; <a href="/wiki/Pinyin" title="Pinyin">pinyin</a>: <i><span lang="zh-Latn-pinyin">Cháng Jiāng nǚshén</span></i>) in China, the dolphin is also called <b>Chinese river dolphin</b>, <b>Yangtze River dolphin</b>, <b>whitefin dolphin</b> and <b>Yangtze dolphin</b>. It was regarded as the goddess of protection by local fishermen and boatmen in China (Zhou, 1991).<sup id="cite_ref-:0_5-0" class="reference"><a href="#cite_note-:0-5">[5]</a></sup> It is not to be confused with the <a href="/wiki/Chinese_white_dolphin" title="Chinese white dolphin">Chinese white dolphin</a> or the <a href="/wiki/Finless_porpoise" title="Finless porpoise">finless porpoise</a>.
-</p>'''
-    result = app.extract_first_link(html)
-    print(result)
-    assert app.extract_first_link(html) == 'River_dolphin'
+def test_extract_links():
+    result = app.extract_links('Germany')
+    assert len(result) > 20
+    assert type(result) == list
 
-
-def test_return_first_link():
-    pass
-
-def test_collect_data():
-    pass
+def test_fetch_all_links():
+    result = app.fetch_all_links('Germany',5)
+    assert result > 3
